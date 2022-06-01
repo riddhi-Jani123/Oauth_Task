30acda714d003199d5583197af56bb4664da6a29
riddhi-jani123 <riddhij.inexture@gmail.com>
2022-06-01 18:51:34 +0530

    first commit

---
+++ src/main/java/com/example/Oath2SecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2SecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2SecurityDemoApplication.class, args);
+	}
+
+}
